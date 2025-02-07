cbb035f296ba732c5ec0497d45ccc8871a372e9c
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-08 00:48:11 +0100

    j'ai termin laclass de connexion data base

---
+++ public/Util.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Utoil{
+
+    // method pour la valeur des input 
+public function testInput($data){
+    $data= trim($data);
+    $data= stripcslashes($data);
+    $data= htmlspecialchars($data);
+    $data= strip_tags($data);
+    return $data;
+
+}
+// methode pour afficher succes et error message
+
+public function showMessage($type,$message){
+    return '<div class="alert alert-'.$type.' alert-dismissible fade show" role="alert">
+  <strong>'.$message.'</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ public/config.php
@@ -0,0 +1,39 @@
+<?php 
+
+class Connexion
+{
+    private static $host = 'db';  // The service name defined in Docker Compose
+    private static $db = 'mydatabase';
+    private static $username = 'user';
+    private static $password = 'password';
+     public static $conn=null;
+
+    static function connexion()
+    {
+        if(self::$conn===null)
+        {
+            try{
+                self::$conn= new PDO("pgsql:host=".self::$host.";dbname=".self::$db,
+                self::$username,
+                self::$password,
+            );
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "3la molana";
+            }catch(PDOExeption $exeption)
+            {
+                die ( " L'error est " . $exeption->getMessage());
+            }
+            return self::$conn;
+
+        }
+        else
+        {
+            return self::$conn;
+        }
+
+    }
+
+
+}
+
+// Connexion::connexion();
