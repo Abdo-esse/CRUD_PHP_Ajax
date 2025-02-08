1ff90fd4022f553b5db4c943de8080ec2e4469db
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-08 18:55:48 +0100

    J'ai termin l'insert du data

--- public/Database.php
+++ public/Database.php
@@ -4,16 +4,25 @@ include './config.php';
 
 class Database{
 
-    public function insert($fname,$lname,$email,$phone){
-        $conn= Connexion::connexion();
-        $sql= 'INSERT into users(first_name,last_name,email,phone) value(:fname,:lname,:email,:phone)';
-        $stm= $conn->prepare($sql);
+    public function insert($fname, $lname, $email, $phone) {
+        if (empty($fname) || empty($lname) || empty($email) || empty($phone)) {
+            throw new Exception("Tous les champs doivent être remplis.");
+        }
+    
+        $conn = Connexion::connexion();
+        $sql = 'INSERT INTO users (first_name, last_name, email, phone) VALUES (:fname, :lname, :email, :phone)';
+        
+        $stm = $conn->prepare($sql);
         $stm->execute([
-            'first_name'=>$fname,
-            'last_name'=>$lname,
-            'email'=>$email,
-            'phone'=>$phone
+            ':fname' => $fname,
+            ':lname' => $lname,
+            ':email' => $email,
+            ':phone' => $phone
         ]);
+    
         return true;
     }
+    
+    
+    
 }
--- public/action.php
+++ public/action.php
@@ -1,10 +1,26 @@
 <?php
 include './Database.php';
 include './Util.php';
-$db=new Database;
-$db=new Util;
 
-// Handle Add New user ajax Request 
-if(isset($_POST['add'])){
-    print_r($_POST);
+$db = new Database;
+$util = new Util;
+
+if (isset($_POST['add'])) {
+    print_r($_POST); // 👀 Débogage : voir les données reçues
+
+    $firstName = $util->testInput($_POST['firstName'] ?? '');
+    $lastName = $util->testInput($_POST['lastName'] ?? '');
+    $email = $util->testInput($_POST['email'] ?? '');
+    $phone = $util->testInput($_POST['phone'] ?? '');
+
+    if (empty($firstName) || empty($lastName) || empty($email) || empty($phone)) {
+        echo $util->showMessage('danger', 'Tous les champs sont obligatoires.');
+        exit;
+    }
+
+    if ($db->insert($firstName, $lastName, $email, $phone)) {
+        echo $util->showMessage('success', 'User inserted successfully!');
+    } else {
+        echo $util->showMessage('danger', 'Something went wrong!');
+    }
 }
